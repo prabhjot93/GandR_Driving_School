98c5b4dbd9334628a8bc92961f8efab1028fd552
prabhjot93 <65588414+prabhjot93@users.noreply.github.com>
2020-08-11 20:36:38 +1000

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,254 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>G$R-About </title>
+<!-- custom-theme -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Driving School" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //custom-theme -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<script src="js/main.js"></script>
+<!-- //js -->
+<!-- font-awesome-icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome-icons -->
+<link href="//fonts.googleapis.com/css?family=Prompt:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext,thai,vietnamese" rel="stylesheet">
+</head>
+<body>
+<!-- banner -->
+<?php include_once('includes/header.php');?>
+    <div class="banner1">
+	</div>
+<!-- //banner -->	
+<!-- about -->
+	<!-- about-top -->
+	<div class="about-top">
+		<div class="container">
+			<div class="w3l-heading">
+				<h2 class="G_head">About Us </h2>
+			</div>
+			<div class="wthree-services-bottom-grids">
+				<div class="col-md-6 wthree-services-left">
+					<img src="images/5.jpg" Class="img-responsive" alt="">
+				</div>
+				<div class="col-md-6 wthree-services-right">
+					<div class="wthree-services-right-top">
+						<h4>We present you the best of us..</h4>
+						<p>We start a small driving school here in brisbane and we are proud to say that now we have braches all over the countyr.</p>
+					</div>
+					<div class="wthree-services-right-bottom">
+						<div class="services-right-bottom-bottom">
+							<div class="services-bottom-icon">
+								<i class="fa fa-bell" aria-hidden="true"></i>
+							</div>
+							<div class="services-bottom-info">
+								<h5> Word from Our Manager</h5>
+								<p>We teach to keep yourself and othes safe on the road.</p>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+						<div class="services-right-bottom-bottom">
+							<div class="services-bottom-icon">
+								<i class="fa fa-asterisk" aria-hidden="true"></i>
+							</div>
+							<div class="services-bottom-info">
+								<h5>Our importance</h5>
+								<p>We teach to keep yourself and othes safe on the road.</p>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!-- //about-top -->
+	<!-- advantages -->
+	<div class="advantages">
+		<div class="agile-dot">
+			<div class="container">
+				<div class="advantages-main">
+					<div class="w3l-heading">
+						<h3 class="G_head">Our Advantages </h3>
+					</div>
+				   <div class="advantage-bottom">
+					 <div class="col-md-4 advantage-grid">
+						<div class="advantage-block">
+							<h3>01</h3>
+							<h4>Safety</h4>
+							<p>We tach to keep yourself and othes safe on the road.</p>
+						</div>
+					 </div>
+					 <div class="col-md-4 advantage-grid">
+						<div class="advantage-block">
+							<h3>02</h3>
+							<h4>Skills</h4>
+							<p>We tach to keep yourself and othes safe on the road.</p>
+						</div>
+					 </div>
+					 <div class="col-md-4 advantage-grid">
+						<div class="advantage-block">
+							<h3>03</h3>
+							<h4>Tech</h4>
+							<p>We tach to keep yourself and othes safe on the road.</p>
+						</div>
+					 </div>
+					<div class="clearfix"> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //advantages -->
+	<!-- team -->
+	<div class="team">
+		<div class="container">
+		   <div class="team-main">
+			   <div class="w3l-heading">
+						<h3 class="G_head">Our Team</h3>
+					</div>   	  
+				<div class="team-bottom">
+					<div class="col-md-3 team-grids">
+						<!-- normal -->
+						<div class="ih-item circle effect5">
+							<div class="img"><img src="images/t1.jpg" alt="img" class="img-responsive"></div>
+							<div class="info">
+							  <div class="info-back">
+								<h3>Instructor 1</h3>		          
+							  </div>			          
+							</div>
+						</div>
+						<div class="team-bottom-info">
+							<p>Words from our instructor.</p>
+							<div class="w3l-social">
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-rss"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						
+						<!-- end normal -->
+					</div>
+					<div class="col-md-3 team-grids">
+						<!-- normal -->
+						<div class="ih-item circle effect5">
+							<div class="img"><img src="images/t2.jpg" alt="img" class="img-responsive"></div>
+							<div class="info">
+							  <div class="info-back">
+								<h3>Instructor 2</h3>		           	            
+							  </div>
+							</div>
+						</div>
+						<div class="team-bottom-info">
+							<p>Words from our instructor.</p>
+							<div class="w3l-social">
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-rss"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					<!-- end normal -->		 
+					</div>
+					<div class="col-md-3 team-grids">
+						<!-- normal -->
+						<div class="ih-item circle effect5">
+							<div class="img"><img src="images/t3.jpg" alt="img" class="img-responsive"></div>
+							<div class="info">
+							  <div class="info-back">
+								<h3>Instructor 3</h3>			       
+							  </div>
+							</div>
+						</div>
+						<div class="team-bottom-info">
+							<p>Words from our instructor.</p>
+							<div class="w3l-social">
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-rss"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<!-- end normal -->
+					</div>
+					<div class="col-md-3 team-grids">
+						<!-- normal -->
+						<div class="ih-item circle effect5">
+							<div class="img"><img src="images/t4.jpg" alt="img" class="img-responsive"></div>
+							<div class="info">
+							  <div class="info-back">
+								<h3>Instructor 4</h3>		           
+							  </div>
+							</div>
+						</div>
+						<div class="team-bottom-info">
+							<p>Words from our instructor.</p>
+							<div class="w3l-social">
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-rss"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<!-- end normal -->			 
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //team -->
+	<!-- //about -->
+
+	<!-- footer -->
+	<?php include_once('includes/footer.php');?>
+	<!-- //footer -->
+
+<!-- start-smooth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smooth-scrolling -->
+<!-- for bootstrap working -->
+	<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+</body>
+</html>
---
+++ user-enquiry.php
@@ -0,0 +1,134 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+if (strlen($_SESSION['cdsmsaid']==0)) {
+  header('location:logout.php');
+  } else{
+   
+
+
+  ?>
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Driving School Enquiry</title>
+    <meta name="description" content="Admin">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="favicon.ico">
+
+
+    <link rel="stylesheet" href="vendors/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/themify-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="vendors/selectFX/css/cs-skin-elastic.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+
+
+</head>
+
+<body>
+    <!-- Left Panel -->
+
+    <?php include_once('includes/sidebar.php');?>
+
+    <div id="right-panel" class="right-panel">
+
+        <!-- Header-->
+        <?php include_once('includes/header.php');?>
+        <div class="breadcrumbs">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>Subscribe Users</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li><a href="dashboard.php">Dashboard</a></li>
+                            <li><a href="subscribed-users.php">Users Enquiry</a></li>
+                            <li class="active">Users</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content mt-3">
+            <div class="animated fadeIn">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <strong class="card-title">Users Enquiry</strong>
+                            </div>
+                            <div class="card-body">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <tr>
+                  <th>S.NO</th>
+            
+                  <th>Full Name</th>
+                   <th>Email</th>        
+                   <th>Action</th>
+                </tr>
+                                        </tr>
+                                        </thead>
+                                    <?php
+$ret=mysqli_query($con,"select *from tblenquiry where Is_Read is null");
+$cnt=1;
+while ($row=mysqli_fetch_array($ret)) {
+
+?>
+              
+                <tr>
+                  <td><?php echo $cnt;?></td>
+            
+                  <td><?php  echo $row['FullName'];?></td>
+                  <td><?php  echo $row['Email'];?></td>
+                  <td><a href="view-user-enquiry.php?enqid=<?php echo $row['ID'];?>">View</a>
+                </tr>
+                <?php 
+$cnt=$cnt+1;
+}?>
+
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div><!-- .animated -->
+        </div><!-- .content -->
+
+
+    </div><!-- /#right-panel -->
+
+    <!-- Right Panel -->
+
+
+    <script src="vendors/jquery/dist/jquery.min.js"></script>
+    <script src="vendors/popper.js/dist/umd/popper.min.js"></script>
+    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+
+</body>
+
+</html>
+<?php }  ?>
